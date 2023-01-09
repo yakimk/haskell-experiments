e6e5ec2cc3b9cb6e0c3fa67a384010c580514662
Rio <yakimenko.kir@gmail.com>
2023-01-09 21:57:41 +0100

    Breaking records problem

---
+++ exercises/Breaking-the-Records.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+main :: IO()
+main = interact $ unwords . map show . solve . map read .  tail . words 
+
+solve :: [Int] -> [Int]
+solve list =[high, low]
+    where
+    high = (length . group . map maximum  . tail $ inits list)-1
+    low = (length . group . map minimum  . tail $ inits list)-1
