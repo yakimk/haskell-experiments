eaa72f8789e76e87362e5cffe80f58fd73db0757
Rio <yakimenko.kir@gmail.com>
2023-01-09 21:41:05 +0100

    Number Line Jumps

---
+++ exercises/Number-Line-Jumps.hs
@@ -0,0 +1,11 @@
+-- https://www.hackerrank.com/challenges/kangaroo/problem
+main :: IO ()
+main = interact $ solve . map read . words 
+
+solve :: [Int] ->String
+solve [x1,v1,x2,v2]
+    | modular == 0 && v2<v1 =  "YES"
+    | otherwise = "NO"
+    where  
+        modular = (x2-x1) `mod` (v1-v2)
+
