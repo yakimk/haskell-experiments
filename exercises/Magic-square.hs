d4ece08ac68583fcc1d312805c0c89ac45008b13
yakimk <yakimenko.kir@gmail.com>
2023-01-21 12:13:59 +0100

    Some bug appeared

--- exercises/Magic-square.hs
+++ exercises/Magic-square.hs
@@ -1,4 +1,5 @@
 -- https://www.hackerrank.com/challenges/magic-square-forming/problem
+
 import Data.List
 
 type MS = [[Int]]
@@ -6,9 +7,12 @@ type MS = [[Int]]
 pp :: MS -> IO()
 pp = putStrLn . unlines . map (unwords . map show) 
 
-applyN :: Read a => Int -> (a -> a) -> a -> a
-applyN 0 f x = x
-applyN n f x = applyN (n-1) f x
+
+
+ref :: MS
+ref = [[7, 6, 5],
+    [7, 2, 8],
+    [5, 3, 4]]
 
 magic :: MS
 magic = [[8,1,6],
@@ -21,16 +25,32 @@ rotate = map reverse . transpose
 refl :: MS -> MS
 refl = transpose . rotate
 
-main :: IO()
-main = undefined
 
 transforms ::  MS -> [MS]
 transforms ms = [ applyN m refl $ applyN n rotate ms | n <- [0..3], m <- [0,1]]
 
 listDif :: [Int] -> [Int] -> Int
-listDif l [] = sum $ map abs l
-listDif [] m = sum $ map abs m
+listDif [] [] = 0
 listDif (k:l) (s:m) = abs(k-s) + listDif l m 
 
 cost :: MS -> MS -> Int
-cost = sum $ map listDif
+cost [] [] = 0
+cost (x:xs) (y:ys) = listDif x y + cost xs ys
+
+solve :: MS -> [Int]
+solve ms = [cost ms ref| ref <- transforms magic]
+
+applyN :: Read a => Int -> (a -> a) -> a -> a
+applyN 0 f x = x
+applyN 1 f x = f x
+applyN n f x = applyN (n-1) f x
+
+getList :: IO[Int]
+getList = map read . words <$> getLine
+
+main :: IO()
+main = do
+    n <- getList
+    m <- getList
+    k <- getList
+    print $ solve [n,m,k]
